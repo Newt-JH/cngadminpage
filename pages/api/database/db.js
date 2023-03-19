9892d3e6afe0ff759dfb290160e865ac6dfe8993
김지환 <newt@avatye.com>
2023-03-19 22:04:13 +0900

    [PLAT-1] DB Conntction Description 추가

--- pages/api/database/db.js
+++ pages/api/database/db.js
@@ -1,3 +1,7 @@
+/*
+* db Connettion 모듈, import 후 dbConect 함수를 사용해 SP 호출
+*/
+
 const mysql = require("mysql2");
 
 let db;
@@ -14,6 +18,8 @@ try {
     console.error(err);
 }
 
+// DB 커넥션 시 SP 제작 후 호출하는 함수
+// query 부분의 경우 SP 이름을 넣으면 되고, DATA의 경우 배열 형식으로 매개변수를 넣어주면 된다.
 const dbConnet = (query, data) => {
     let formattedQuery = query;
     let Returndata = {
